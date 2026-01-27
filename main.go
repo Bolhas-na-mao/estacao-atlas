1a1c10f49af35e7541bcb44b7966ad40e3846133
Bruno Aseff <brunoaseff2@gmail.com>
2026-01-27 01:09:14 -0300

    refactor: improve grid layout

--- main.go
+++ main.go
@@ -10,23 +10,39 @@ import (
 
 type Game struct{}
 
-const SCREEN_WIDTH = 640
+const SCREEN_WIDTH = 720
 const SCREEN_HEIGHT = 480
 const SQUARE_WIDTH = 30
+const PADDING = 10
 
 func (g *Game) Update() error {
 	return nil
 }
 
 func DrawGrid(screen *ebiten.Image) {
+	availWidth := SCREEN_WIDTH - (PADDING * 2)
+	availHeight := SCREEN_HEIGHT - (PADDING * 2)
 
-	for i := 0; i < SCREEN_WIDTH; i += SQUARE_WIDTH {
-		for j := 0; j < SCREEN_HEIGHT; j += SQUARE_WIDTH {
+	cols := availWidth / SQUARE_WIDTH
+	rows := availHeight / SQUARE_WIDTH
+
+	totalGridWidth := cols * SQUARE_WIDTH
+	totalGridHeight := rows * SQUARE_WIDTH
+
+	startX := (SCREEN_WIDTH - totalGridWidth) / 2
+	startY := (SCREEN_HEIGHT - totalGridHeight) / 2
+
+	for i := 0; i < cols; i++ {
+		for j := 0; j < rows; j++ {
+
+			x := startX + (i * SQUARE_WIDTH)
+			y := startY + (j * SQUARE_WIDTH)
+
 			vector.StrokeRect(
 				screen,
-				float32(i), float32(j),
+				float32(x), float32(y),
 				SQUARE_WIDTH, SQUARE_WIDTH,
-				1,
+				2,
 				color.RGBA{217, 218, 224, 255},
 				false,
 			)
