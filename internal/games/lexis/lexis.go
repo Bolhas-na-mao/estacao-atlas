e9d7b864492b5eddfc02863624a9aead22614f27
Bruno Aseff <brunoaseff2@gmail.com>
2026-02-01 13:01:26 -0300

    feat: render hero on screen

--- internal/games/lexis/lexis.go
+++ internal/games/lexis/lexis.go
@@ -1,6 +1,7 @@
 package lexis
 
 import (
+	"embed"
 	"fmt"
 	"image/color"
 
@@ -9,6 +10,9 @@ import (
 )
 
 var startButton *ui.Button
+
+//go:embed assets/*
+var assets embed.FS
 
 func init() {
 	startButton = ui.NewButton(100, 100, 200, 50, "Lexis...")
@@ -25,6 +29,14 @@ func Update() error {
 func Run(screen *ebiten.Image) error {
 	screen.Fill(color.RGBA{0, 0, 0, 255})
 	startButton.Draw(screen)
+
+	img, err := ui.RenderAsset(assets, "assets/characters/hero/idle/east.png")
+
+	if err != nil {
+		return err
+	}
+
+	screen.DrawImage(img, nil)
 
 	return nil
 }
