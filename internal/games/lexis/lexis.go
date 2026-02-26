5287ae8278c6a9bb21f55e3e4109d74795e8c6a8
Bruno Aseff <brunoaseff2@gmail.com>
2026-02-26 14:06:06 -0300

    feat: update lexis

--- internal/games/lexis/lexis.go
+++ internal/games/lexis/lexis.go
@@ -26,7 +26,9 @@ func init() {
 }
 
 type LexisGame struct {
-	hero *Player
+	hero   *Player
+	theMap Map
+	camera Camera
 }
 
 func New() *LexisGame {
@@ -44,15 +46,40 @@ func New() *LexisGame {
 	walkingSpritesheets[Right] = rightWalk
 
 	leftWalk, err := ui.RenderAsset(assets, "assets/hero/walking_left.png")
-
 	if err != nil {
 		log.Fatal(err)
 	}
 	walkingSpritesheets[Left] = leftWalk
 
-	hero := newPlayer(idleSpritesheet, walkingSpritesheets, Right, "Hero", 200, 200)
+	hero := newPlayer(idleSpritesheet, walkingSpritesheets, Right, "Hero", 30, 20)
 
-	return &LexisGame{hero: hero}
+	lib1, err := ui.RenderAsset(assets, "assets/rooms/library_1.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lib2, err := ui.RenderAsset(assets, "assets/rooms/library_2.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	golemSheet, err := ui.RenderAsset(assets, "assets/npcs/golem.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	golem := newGolem(golemSheet, "Golem", 390, 20)
+
+	theMap := Map{
+		rooms: []Room{
+			{sprite: lib1, worldX: 0},
+			{sprite: lib2, worldX: roomWidth, npcs: []Npc{*golem}},
+		},
+	}
+
+	return &LexisGame{
+		hero:   hero,
+		theMap: theMap,
+		camera: Camera{},
+	}
 }
 
 func (g *LexisGame) Update() error {
@@ -67,7 +94,15 @@ func (g *LexisGame) Update() error {
 	}
 	g.hero.move(dx)
 
+	if g.hero.x < 0 {
+		g.hero.x = 0
+	}
+	if g.hero.x > worldWidth-spriteWidth {
+		g.hero.x = worldWidth - spriteWidth
+	}
+
 	g.hero.update()
+	g.camera.update(g.hero.x, worldWidth)
 
 	return nil
 }
@@ -75,9 +110,11 @@ func (g *LexisGame) Update() error {
 func (g *LexisGame) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{0, 0, 0, 255})
 
+	g.theMap.draw(screen, &g.camera)
+
+	sx, sy := g.camera.toScreen(g.hero.x, g.hero.y)
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Scale(heroScale, heroScale)
-	op.GeoM.Translate(g.hero.x, g.hero.y)
-
+	op.GeoM.Translate(sx, sy)
 	screen.DrawImage(g.hero.currentImage(), op)
 }
