784a5d500b2fef2136f75c9277ed4d4a69908608
Bruno Aseff <brunoaseff2@gmail.com>
2026-02-26 14:05:50 -0300

    feat: add draw method inside map

--- internal/games/lexis/map.go
+++ internal/games/lexis/map.go
@@ -2,9 +2,39 @@ package lexis
 
 import "github.com/hajimehoshi/ebiten/v2"
 
+const (
+	roomWidth   = 256
+	roomHeight  = 112
+	worldWidth  = roomWidth * 2
+	worldHeight = roomHeight
+)
+
 type Room struct {
 	sprite *ebiten.Image
 	npcs   []Npc
+	worldX float64
 }
 
-type Map []Room
+type Map struct {
+	rooms []Room
+}
+
+func (m *Map) draw(screen *ebiten.Image, cam *Camera) {
+	for i := range m.rooms {
+		room := &m.rooms[i]
+		sx, sy := cam.toScreen(room.worldX, 0)
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(heroScale, heroScale)
+		op.GeoM.Translate(sx, sy)
+		screen.DrawImage(room.sprite, op)
+
+		for j := range room.npcs {
+			npc := &room.npcs[j]
+			nx, ny := cam.toScreen(npc.x, npc.y)
+			nop := &ebiten.DrawImageOptions{}
+			nop.GeoM.Scale(heroScale, heroScale)
+			nop.GeoM.Translate(nx, ny)
+			screen.DrawImage(npc.currentImage(), nop)
+		}
+	}
+}
